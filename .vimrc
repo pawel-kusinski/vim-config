209b62fc92a285b236f55437da937b05718e5177
Pawel Kusinski <pawel@kusinski.net>
2024-11-07 14:11:28 +0400

    Install coc

--- .vimrc
+++ .vimrc
@@ -16,8 +16,20 @@ Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
 Plug 'junegunn/fzf.vim'
 Plug 'vim-airline/vim-airline'
 Plug 'vim-airline/vim-airline-themes'
+Plug 'neoclide/coc.nvim', {'branch': 'release'}
 
 call plug#end()
 
 nnoremap <C-p> :Files<Cr>
+
+" use <tab> to trigger completion and navigate to the next complete item
+function! CheckBackspace() abort
+  let col = col('.') - 1
+  return !col || getline('.')[col - 1]  =~# '\s'
+endfunction
+
+inoremap <silent><expr> <Tab>
+      \ coc#pum#visible() ? coc#pum#next(1) :
+      \ CheckBackspace() ? "\<Tab>" :
+      \ coc#refresh()
 
--- setup_vim.sh
+++ setup_vim.sh
@@ -6,6 +6,12 @@ REPO_DIR="$(dirname "$(realpath "$0")")"
 # Download and install Vim-Plug (plugin manager)
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install nodejs for coc
+curl -sL install-node.vercel.app/lts | bash
+
+# Install clangd for C/C++ coc
+sudo apt install -y clangd
+
 # Backup existing .vimrc if it exists
 if [ -f ~/.vimrc ]; then
     mv ~/.vimrc ~/.vimrc.backup
@@ -15,8 +21,23 @@ fi
 # Create symbolic link to the repository's .vimrc
 ln -s "$REPO_DIR/.vimrc" ~/.vimrc
 
+# Create the config directory if it doesn't exist
+mkdir -p ~/.vim/config/
+
+# Backup the existing coc-settings.json if it exists
+if [ -f ~/.vim/coc-settings.json ]; then
+    mv ~/.vim/coc-settings.json ~/.vim/coc-settings.json.backup
+    echo "Backed up existing coc-settings.json"
+fi
+
+# Create symbolic link to the repository's coc-settings.json
+ln -s "$REPO_DIR/coc-settings.json" ~/.vim/coc-settings.json
+
 # Open Vim, run the PlugInstall command to install all plugins listed in .vimrc,
 # and then automatically quit Vim once the installation is complete.
 vim +PlugInstall +qall
+
+# Install coc-clangd for C/C++ support in coc.nvim
+vim +'CocInstall -sync coc-clangd' +qall
 
 echo "Vim configured!"
